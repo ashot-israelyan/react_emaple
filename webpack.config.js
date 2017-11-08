9ae61621595623741203dc14c8bc03b00ec1183e
Ashot Israelyan <ashot.israelyan@picsart.com>
2017-11-08 18:08:03 +0400

    Webpack improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,52 @@
-module.exports = {
-    entry: './public/app.js',
-    output: {
-        path: __dirname + '/public',
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-                exclude: /(node_modules)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            }
-        ]
-    },
-    watch: true
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = env => {
+    const isProdution = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return {
+        entry: ['babel-polyfill', './src/app.js'],
+        output: {
+            path: path.join(__dirname, 'public', 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProdution ? 'source-map': 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true,
+            publicPath: '/dist/'
+        }
+    };
 };
