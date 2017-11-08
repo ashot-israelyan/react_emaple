a79c2b6b2772c9f1e67c06a41ac11a8d9a751702
Ashot Israelyan <ashot.israelyan@picsart.com>
2017-11-08 18:20:49 +0400

    Webpack improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,19 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-module.exports = env => {
-    const isProdution = env === 'production';
-    const CSSExtract = new ExtractTextPlugin('styles.css');
-
-    return {
-        entry: ['babel-polyfill', './src/app.js'],
-        output: {
-            path: path.join(__dirname, 'public', 'dist'),
-            filename: 'bundle.js'
-        },
-        module: {
-            rules: [{
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname + '/public/dist',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                exclude: /(node_modules)/,
                 loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            }, {
-                test: /\.s?css$/,
-                use: CSSExtract.extract({
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        }
-                    ]
-                })
-            }]
-        },
-        plugins: [
-            CSSExtract
-        ],
-        devtool: isProdution ? 'source-map': 'inline-source-map',
-        devServer: {
-            contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true,
-            publicPath: '/dist/'
-        }
-    };
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    watch: true
 };
